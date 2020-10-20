0b5fa5b6b6bce12e8c5d376685ec03e0c285af84
Marwen Aouiti <marwen.aouiti@outlook.fr>
2020-10-20 09:49:26 +0100

    backend port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const Post = require('./models/Post');
 const { MONGODB } = require('./config');
 const typeDefs = require('./graphql/typedefs');
 const resolvers = require('./graphql/resolvers');
 
 const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
@@ -19,7 +20,7 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to DB');
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
